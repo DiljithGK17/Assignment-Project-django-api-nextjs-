e46183434a0aa8a2d956c2c43322f88659fe9dfd
Ubuntu <ubuntu@ip-172-31-4-230.ec2.internal>
2025-07-10 16:41:28 +0000

    Third commit

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
+// frontend/next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone', // This is essential for standalone builds
+  // Remove the entire webpack block for now, as it's not suppressing this error
+  // webpack: (config, { isServer }) => { ... }
+};
 
 module.exports = nextConfig;
